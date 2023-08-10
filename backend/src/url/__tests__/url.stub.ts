ac4329eb5205a2d7ddbb879508f52425f43f8d30
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-08-10 23:15:27 +0800

    tests(backend): change interface to class

--- backend/src/url/__tests__/url.stub.ts
+++ backend/src/url/__tests__/url.stub.ts
@@ -1,5 +1,5 @@
 import { ExecutionContext } from '@nestjs/common';
-import { CreateQrCodeDto, IQrCode, IUrl } from '../url.model';
+import { CreateQrCodeDto, QrCode, Url } from '../url.model';
 
 export const AuthGuardMock = {
   canActivate(context: ExecutionContext) {
@@ -13,12 +13,12 @@ export const AuthGuardMock = {
 
 export const RedirectUrlStub = () => 'http://localhost:3000';
 export const RedirectHashStub = () => 'abcef';
-export const QrCodeStub = (fgColor?: string): IQrCode => ({
+export const QrCodeStub = (fgColor?: string): QrCode => ({
   isCreated: true,
   fgColor: fgColor || '#000',
 });
 
-export const UrlStub = (): IUrl[] => [
+export const UrlStub = (): Url[] => [
   {
     url: 'http://localhost:3000',
     qrCode: QrCodeStub(),
