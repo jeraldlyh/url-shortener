3fb54fc204219edf5577f91ff63b58942d57538c
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-08-11 21:10:30 +0800

    style(web): prevent url from wrapping in mobile viewport

--- web/src/components/Dashboard.tsx
+++ web/src/components/Dashboard.tsx
@@ -211,7 +211,7 @@ export const Dashboard = () => {
                 <span className="flex items-center space-x-3 text-sm md:text-lg">
                   <BiLink />
                   <a
-                    className="link-secondary link max-w-[200px] shrink overflow-hidden text-ellipsis md:max-w-none"
+                    className="link-secondary link max-w-[200px] shrink overflow-hidden text-ellipsis whitespace-nowrap md:max-w-none"
                     href={originalUrl}
                     target="_blank"
                   >
@@ -221,7 +221,7 @@ export const Dashboard = () => {
                 <span className="flex items-center space-x-3 text-sm md:text-lg">
                   <FaLocationArrow />
                   <a
-                    className="link-primary link max-w-[200px] shrink overflow-hidden text-ellipsis md:max-w-none"
+                    className="link-primary link max-w-[200px] shrink overflow-hidden text-ellipsis whitespace-nowrap md:max-w-none"
                     href={redirectUrl}
                     target="_blank"
                   >
